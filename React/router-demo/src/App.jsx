2279912ec53ad4dc073c0a69fdf72fc3647b4481
xxuzzwz <3189315662@qq.com>
2025-07-12 13:29:01 +0800

    feat(router-demo): 实现路由组件的按需加载
    - 引入 lazy 和 Suspense 组件，实现路由组件的懒加载
    - 修改路由配置，使用 Suspense 包裹 Routes 以显示加载状态
    - 优化代码格式，提高可读性

--- React/router-demo/src/App.jsx
+++ React/router-demo/src/App.jsx
@@ -3,20 +3,26 @@ import {
   Routes, // 路由容器，用来包裹所有的路由规则
   Route // 单个路由规则，定义路径和对应的组件
 } from 'react-router-dom'
-import  Home  from './pages/Home'
-import  About  from './pages/About'
+import Home from './pages/Home'
+import About from './pages/About'
+import { lazy, Suspense } from 'react'
+
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
 
 function App() {
- 
+
   return (
     <>
-    {/* 前端路由接管一切，配置 */}
-     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-      </Routes>
-     </Router>
+      {/* 前端路由接管一切，配置 */}
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
+      </Router>
     </>
   )
 }
