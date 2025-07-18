c18c5964366f6fd9259c5e3a684264f4f3025f5e
XXUZZWZ <3189315662@qq.com>
2025-07-18 19:08:21 +0800

    feat(RepoList): 实现仓库列表页面
    - 添加 RepoList 组件
    - 使用 useRepos 钩子获取仓库数据
    - 渲染仓库列表
    - 添加导航到单个仓库页面的功能

--- React/react-repos/src/pages/RepoList/RepoList.jsx
+++ React/react-repos/src/pages/RepoList/RepoList.jsx
@@ -1,8 +1,42 @@
+import { useParams,Navigate,useNavigate } from "react-router-dom"
+import {
+  useEffect
+}from "react"
+import {Link}from "react-router-dom"
+import {
+  useRepos
+}from '../../hooks/useRepos'
 const RepoList = ()=>{
+  const {id} = useParams()
+  console.log(id)
+  const navigate = useNavigate();
+  const state =  useRepos(id);
+  let  {repos,loading,error} = state;
+  
+
+  // console.log(repos,loading,error)
+  // hooks 
+  // useEffect(()=>{
+  //   if(!id.trim()) {
+  //     navigate("/")
+  //     return;
+  //    }
+  // },[]);
+  // if(loading) return <div>Loading...</div>
+  // if(error) return <div>{error}...</div>
   return (
-    <div>
-      RepoList 
-    </div>
+    <>
+        <h1>Repositories for {id}</h1>
+        {
+         repos.map((repo)=>(
+          <div key={repo.id+Date.now()}>
+            <Link to={`/repo/${id}/${repo.name}`}>
+              <h2>{repo.name}</h2>
+            </Link>
+          </div>
+         ))
+        }
+        </>
   )
 }
 
