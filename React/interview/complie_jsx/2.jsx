e310f6b725d6951e143fef1b7918d09512c2b5f2
XXUZZWZ <3189315662@qq.com>
2025-07-21 19:08:42 +0800

    refactor(React): 优化 JSX 元素的创建方式
    - 为 li 元素添加了 key 属性，以提高渲染性能
    - 调整了 JSX 代码的格式，使其更加清晰易读

--- React/interview/complie_jsx/2.jsx
+++ React/interview/complie_jsx/2.jsx
@@ -9,9 +9,12 @@ console.log(a);
 // })();
 
 var element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello, world!"), /*#__PURE__*/React.createElement("h2", null, "It is ", new Date().toLocaleTimeString(), "."));
-var element2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", {
+var element2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/
+  React.createElement("li",
+   {
   key: "abx1"
-}, "1"), /*#__PURE__*/React.createElement("li", {
+}, "1"),
+ /*#__PURE__*/React.createElement("li", {
   key: "abx2"
 }, "2"), /*#__PURE__*/React.createElement("element", null), /*#__PURE__*/React.createElement("element2", null), /*#__PURE__*/React.createElement("li", {
   key: "abx3"
