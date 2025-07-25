bf7e2b7f4fca3937626fd544fe4121dde7e75f80
XXUZZWZ <3189315662@qq.com>
2025-07-25 20:18:43 +0800

    feat(Account): 完善个人中心页面功能
    - 添加个人中心页面的基本组件和布局
    - 集成 useTitle 自定义钩子设置页面标题
    - 使用 react-vant 组件库实现服务、收藏、朋友圈和设置等功能
    - 添加用户信息状态管理

--- React/trip/src/pages/Account/index.jsx
+++ React/trip/src/pages/Account/index.jsx
@@ -1,7 +1,37 @@
-const Account = ()=>{
+import {
+  useTitle
+} from '@/hooks/useTitle'
+import { ServiceO, FriendO, StarO, SettingO, UserO, UserCircleO } from '@react-vant/icons'
+import {
+  useState
+} from 'react'
+import {
+  Image,
+  Cell,
+  CellGroup,
+  ActionSheet,
+  Popup,
+  Loading,
+} from 'react-vant'
+const Account = () => {
+  useTitle('我的')
+  const [userInfo, setUserInfo] = useState({
+    nickname: '奶龙',
+    slogan: '我是奶龙',
+    avatar: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.Pq8dDCkuj2fBhQsiC6A7WgHaD_?r=0&o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3'
+  });
   return (
     <div>
-      Account
+      <CellGroup>
+        <Cell title="服务" icon={<ServiceO />} ></Cell>
+      </CellGroup >
+      <CellGroup inset style={{ marginTop: 8 }}>
+        <Cell title="收藏" icon={<StarO />} isLink />
+        <Cell title="朋友圈" icon={<FriendsO />} isLink />
+      </CellGroup>
+      <CellGroup inset style={{ marginTop: 8 }}>
+        <Cell title="设置" icon={<SettingO />} isLink />
+      </CellGroup>
     </div>
   )
 }
