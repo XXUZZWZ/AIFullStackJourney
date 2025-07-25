7a99eff4abab2dc7d0664ecab416f5f2e68c20a1
XXUZZWZ <3189315662@qq.com>
2025-07-25 16:01:23 +0800

    feat(trip): 添加奶龙客服功能
    - 使用 useTitle 自定义标题为"奶龙客服"
    - 添加聊天功能，使用 kimiChat 进行对话
    - 在页面中显示"Trip"替代原有的"Login"

--- React/trip/src/pages/Trip/index.jsx
+++ React/trip/src/pages/Trip/index.jsx
@@ -1,7 +1,25 @@
+import useTitle from '@/hooks/useTitle'
+import {
+  useEffect
+}from 'react'
+import {chat,kimiChat }from '@/llm'
 const Trip = ()=>{
+  useTitle('奶龙客服')
+ useEffect(()=>{
+  const fecthChat = async ()=>{
+    const res = await kimiChat([
+      {
+        role: 'user',
+        content: '请用中文回答，你叫什么名字？'
+      }
+    ])
+    console.log(res)
+  }
+  fecthChat();
+ })
   return (
     <div>
-      Login
+     Trip 
     </div>
   )
 }
