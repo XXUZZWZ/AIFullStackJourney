67d901db2d8e163545fb259bddee25b79a8485f2
XXUZZWZ <3189315662@qq.com>
2025-07-28 17:49:46 +0800

    refactor(Search): 重构搜索页面并添加搜索框组件
    - 引入 SearchBox 组件以增强页面功能
    - 使用 useCallback 优化

--- React/trip/src/pages/Search/index.jsx
+++ React/trip/src/pages/Search/index.jsx
@@ -1,7 +1,18 @@
-const Search = ()=>{
+import SearchBox from "../../components/SearchBox"
+import { useCallback } from "react"
+
+const Search = () => {
+  // 单向数据流
+  // 函数会反复生成
+  // const handleQuery = () => {
+  //   //处理api 请求
+  // }
+  const handleQuery = useCallback(() => {
+    //处理api 请求
+  })
   return (
     <div>
-      Search
+      <SearchBox handleQuery={handleQuery} />
     </div>
   )
 }
