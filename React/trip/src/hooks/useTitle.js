74a4efd30edacff01e2c673d96c666a810a0d4e7
XXUZZWZ <3189315662@qq.com>
2025-07-29 17:09:18 +0800

    refactor: 移除 useTitle hook 中的 useEffect
    移除了 useTitle hook 中的 useEffect，使设置 document.title 的逻辑直接在函数体中执行。
    这个修改可能是为了简化代码结构，但需要注意它可能会导致一些潜在的性能问题或不期望的行为。

--- React/trip/src/hooks/useTitle.js
+++ React/trip/src/hooks/useTitle.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 function useTitle(title) {
-  useEffect(() => {
-    document.title = title;
-  }, []);
+  // useEffect(() => {
+  document.title = title;
+  // }, []);
 }
 export default useTitle;
