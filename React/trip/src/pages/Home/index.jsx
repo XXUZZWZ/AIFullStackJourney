c1b0c0774020d0c696b12b5afe55c511500fecbf
XXUZZWZ <3189315662@qq.com>
2025-07-29 18:20:22 +0800

    feat(首页): 添加搜索功能并优化布局
    - 在首页添加搜索组件，实现关键词搜索功能
    - 优化页面布局，增加导航栏样式
    - 使用 react-vant 的 Search 组件替换原有的 SearchBox 组件
    - 实现搜索关键词时跳转到搜索结果页面

--- React/trip/src/pages/Home/index.jsx
+++ React/trip/src/pages/Home/index.jsx
@@ -1,11 +1,20 @@
 import useTitle from '@/hooks/useTitle'
-import SearchBox from '../../components/SearchBox';
+import { Search } from 'react-vant';
+import { Navigate, useNavigate } from 'react-router-dom';
 const Home = () => {
   useTitle('奶龙首页');
+  const navigate = useNavigate();
+  const handleChange = (value) => {
+    console.log(value,"fdfsfsdf");
+    navigate('/search', { state: { keyword: value } });
+  };
   return (
     <div>
-      Home
-      <SearchBox />
+
+      <Search
+        placeholder='搜索'
+        onClick={handleChange}
+      ></Search>
     </div>
   )
 }
