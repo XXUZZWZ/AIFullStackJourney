12510da55aa07f5f9b0bca120c2249d1b716b9cd
XXUZZWZ <3189315662@qq.com>
2025-07-29 17:13:24 +0800

    perf(Detail): 移除轮播图中的图片懒加载
    - 在 Swiper 组件中移除了 Image 组件的 lazyload 属性
    - 此修改旨在优化轮播图的加载性能，减少用户等待时间

--- React/trip/src/pages/Detail/index.jsx
+++ React/trip/src/pages/Detail/index.jsx
@@ -70,7 +70,7 @@ const Detail = () => {
           {
             detail.images.map((item, index) => (
               <Swiper.Item key={index}>
-                <Image lazyload src={item.url} />
+                <Image src={item.url} />
               </Swiper.Item>
             ))
           }
