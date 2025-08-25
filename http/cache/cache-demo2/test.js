53825f40418c16f5dfe76d3d8c76c0b59c796d33
XXUZZWZ <3189315662@qq.com>
2025-08-25 16:27:30 +0800

    feat(http): 添加协商缓存示例项目
    - 新增 cache-demo2 项目，演示使用 ETag 实现协商缓存
    - 添加 index.js 作为主入口，实现缓存逻辑
    - 创建 test.html 和 test.js 作为测试页面和脚本
    - 设置 up-server 脚本和 nodemon 配置，方便开发调试

---
+++ http/cache/cache-demo2/test.js
@@ -0,0 +1,9 @@
+// test.js
+console.log("test.js");
+let colors = ["#FFB6C1", "#87CEFA", "#90EE90", "#FFD700", "#FFA07A", "#DDA0DD"];
+let idx = 0;
+setInterval(() => {
+  document.body.style.backgroundColor = colors[idx % colors.length];
+  idx++;
+}, 1000);
+// npx create-next-app@latest my-todos
