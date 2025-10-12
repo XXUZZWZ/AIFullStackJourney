905ed1f8c6886621ada4daee436694b4ee9c21d4
XXUZZWZ <3189315662@qq.com>
2025-10-12 16:30:05 +0800

    feat(filter): 新增数组过滤功能示例
    - 创建 filter.js 文件，包含过滤大于 2 的数的基本逻辑
    - 添加 readme.md 文件，概述数组过滤的功能和实现思路

---
+++ interview/algorithm/filter/1.js
@@ -0,0 +1,17 @@
+// function filter(array) {
+//   return array.filter((item) => {
+//     return item > 2;
+//   });
+// }
+
+// console.log(filter([2, 3, 4, 7, 1, 4]));
+
+//  不能优化 如果 可以是有序的就可以
+
+// 如果是有序数组的话就要 二分
+
+const arr = [1, 2, 3, 5, 6, 8];
+
+const index = arr.findIndex((n) => num > 2);
+
+function filter() {}
